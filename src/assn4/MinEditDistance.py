c3fc54b5912bce969ea2be8873a79ef7788acebd
Bryson <bryson.meiling@aggiemail.usu.edu>
2020-02-02 14:21:01 -0700

    starting analysis of dna. Also need to write program to wirte out all the exceat changes

--- src/assn4/MinEditDistance.py
+++ src/assn4/MinEditDistance.py
@@ -1,5 +1,10 @@
 import numpy as np
 
+
+A = ''
+B = ''
+
+
 # dynamic program is fast
 def med_dp(i, j):
 	# initialize cache with base case along axis
@@ -10,9 +15,20 @@ def med_dp(i, j):
 		c[0, y] = y
 
 	# fill in cache
-	for x in range(1, i+1):
-		for y in range(1, j+1):
-			c[x, y] = min(c[x-1, y] + 1, c[x, y-1] + 1, c[x-1, y-1] + (A[x] != B[y]))
+	try:
+		for x in range(1, i+1):
+
+			if x % 100 == 0:
+				print(f"{x} of {i} done")
+			for y in range(1, j+1):
+				# deletion from A, insertion into A, substitution
+				c[x, y] = max(c[x-1, y] + dna_scoring_dict[A[x]][''],
+								c[x, y-1] + dna_scoring_dict[''][B[y]],
+								c[x-1, y-1] + dna_scoring_dict[A[x]][B[y]])
+
+	except KeyError as e:
+		print('Sequence contains character other than a, g, c, t')
+		print(e.__str__())
 
 	# return answer
 	return c[i, j]
@@ -32,5 +48,26 @@ def med(str_a, str_b):
 	return med_dp(len(A) - 1, len(B) - 1)
 
 
-A = ''
-B = ''
+'''
+Point system when substituting one nucleotide for another nucleotide. 
+d represents a deletion from the sequence
+	A  C  G  T  d
+A   5 -1 -2 -1 -3
+C  -1  5 -3 -2 -4
+G  -2 -3  5 -2 -2
+T  -1 -2 -2  5 -1
+d  -3 -4 -2 -1  *
+'''
+dna_scoring_table = np.array([
+	[5, -1, -2, -1, -3],
+	[-1, 5, -3, -2, -4],
+	[-2, -3, 5, -2, -2],
+	[-1, -2, -2, 5, -1],
+	[-3, -4, -2, -1, 0]
+])
+dna_scoring_dict = {'a': {'a': 5, 'c': -1, 'g': -2, 't': -1, '': -3},
+					'c': {'a': -1, 'c': 5, 'g': -3, 't': -2, '': -4},
+					'g': {'a': -2, 'c': -3, 'g': 5, 't': -2, '': -2},
+					't': {'a': -1, 'c': -2, 'g': -2, 't': 5, '': -1},
+					'': {'a': -3, 'c': -4, 'g': -2, 't': -1, '': 0}}
+
--- src/assn4/main.py
+++ src/assn4/main.py
@@ -1,7 +1,11 @@
 import pathlib2 as path
+from MinEditDistance import med
+
 
 neanderthal = 'neanderthal.txt'
 prototypical_human = 'prototypical_human.txt'
+great_apes = 'Great_Apes.txt'
+human_diversity = 'human_diversity.txt'
 
 
 def read_dna(filename: str)->str:
@@ -11,4 +15,21 @@ def read_dna(filename: str)->str:
 	return dna.strip('\n')
 
 
-print(read_dna(prototypical_human))
+def clean_data(to_file: str, from_file:str ='new.txt'):
+	reciever = open(path.Path.joinpath(path.Path.cwd(), 'data', to_file), 'w+')
+	with open(path.Path.joinpath(path.Path.cwd(), 'data', from_file), 'r') as sender:
+
+		lines = sender.readlines()
+		s = ''
+		for line in lines:
+			broken = line.strip().split()
+			for chunk in broken[1:]:
+				s += chunk
+
+		reciever.write(s)
+		reciever.close()
+
+
+apes = read_dna(great_apes)
+hm = read_dna(human_diversity)
+print(med(apes, hm))
