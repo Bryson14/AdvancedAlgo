e14e02a4bf6169f9b71ab13b95b1f5df23d90660
Bryson <bryson.meiling@aggiemail.usu.edu>
2020-03-25 12:32:31 -0600

    this stuff is really starting to get to me lol

--- src/assn7_fast_fourier_transform/FFT.py
+++ src/assn7_fast_fourier_transform/FFT.py
@@ -1,42 +1,62 @@
-from numpy import exp, pi, random, array, fft, zeros, concatenate, arange
+from numpy import exp, pi, random, array, fft, zeros, concatenate, arange, allclose, conjugate
 
 
-# used for removing computationally unstable numbers
-lim = 1e-16
-
-
-def some_fft(p, n):
+# fast fourier transform
+def my_fft(p, n):
     if do_print:
         print(f"in = {p}")
     if n == 1:
         if do_print:
             print(f"out = {p}")
         return p
-    even = some_fft(p[0::2], n//2)
-    odd = some_fft(p[1::2], n//2)
+    even = my_fft(p[0::2], n // 2)
+    odd = my_fft(p[1::2], n // 2)
     factor = exp(-2j * pi * arange(n) / n)
-    return concatenate([even + factor[:n//2] * odd, even - factor[n//2:] * odd])
+    return concatenate([even + factor[:n//2] * odd, even + factor[n//2:] * odd])
 
 
+# inverse fast fourier transform
+def my_ifft(p, n):
+    if do_print:
+        print(f"in = {p}")
+    if n == 1:
+        if do_print:
+            print(f"out = {p}")
+        return p
+    even = my_ifft(p[0::2], n // 2)
+    odd = my_ifft(p[1::2], n // 2)
+    factor = exp(2j * pi * arange(n // 2) / (n//2))
+    return concatenate([even + factor * odd, even - factor * odd])*2/n
+
+
+# adds buffer so that length of arr is a factor of 2. i.e. len(arr) % 2 == 0
 def add_buffer(arr):
     i = 0
-    done = False
-    while not done:
+    n = 0
+    while n != len(arr):
         n = 2**i
         # requires a buffer to have len of power of 2
         if n > len(arr):
-            add_on = zeros([n - len(arr), ])
-            return concatenate((arr, add_on))
-        # arr is already a power of 2 length
-        elif n == len(arr):
-            return arr
+            return concatenate((arr, zeros([n - len(arr), ])))
+
         # N is still smaller than arr length
         else:
             i += 1
 
+    return arr
+
+
+def fft_helper(arr, function):
+    arr = add_buffer(arr)
+    return function(arr, len(arr))
+
 
 do_print = False
 
-arr = array([0,1,2,3,4,5,6,7])
+arr = array([0,1,2,3,4,5,6,7], dtype=complex)
 print(fft.fft(arr))
-print(some_fft(arr,8))
+print('mine')
+print(fft_helper(arr, my_fft))
+print("It's close enough? : ", allclose(fft_helper(arr, my_fft), fft.fft(arr)))
+print(fft_helper(fft.fft(arr), my_ifft))
+
--- src/assn7_fast_fourier_transform/test_main.py
+++ src/assn7_fast_fourier_transform/test_main.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
-from numpy import array
-from main import add_buffer
+from numpy import array, fft, allclose
+from FFT import add_buffer, my_fft, fft_helper
 
 
 class Test(TestCase):
@@ -20,3 +20,13 @@ class Test(TestCase):
         self.assertTrue((arr2_1 == add_buffer(arr2)).all())
         self.assertTrue((arr3_1 == add_buffer(arr3)).all())
         self.assertTrue((arr4_1 == add_buffer(arr4)).all())
+
+
+    def test_my_fft(self):
+        arr1 = array([0,1,2,3,4,5,6,7])
+        arr2 = array([5,1,3,4])
+        arr3 = array([0])
+
+        self.assertTrue(allclose(fft.fft(arr1), fft_helper(arr1, my_fft)))
+        self.assertTrue(allclose(fft.fft(arr2), fft_helper(arr2, my_fft)))
+        self.assertTrue(allclose(fft.fft(arr3), fft_helper(arr3, my_fft)))
