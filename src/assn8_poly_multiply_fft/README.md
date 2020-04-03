5f8d38522c55797d912c5bfaf482199ffb447a39
Bryson <bryson.meiling@aggiemail.usu.edu>
2020-04-02 23:28:59 -0600

    all but correct fft multiply done and error analysis

--- src/assn8_poly_multiply_fft/README.md
+++ src/assn8_poly_multiply_fft/README.md
@@ -5,13 +5,18 @@ python poly_multiply.py --> runs the analysis
 TODO add some other stuff here
 
 ## Intro
-This algorithm builds on past aassignments and tests the speed of three algorithms when multiplying very large integers.
+This algorithm builds on past assignments and tests the speed of three algorithms when multiplying very large integers.
 The three algorithms are:
 
 * High School Algorithm (FOIL)
-* Three-subproblem Algorithm
+* Three-sub problem Algorithm
 * FFT
 
 ## Results
-TBD
+1) The high school algorithm, three-sub algorithm, and fft multiply all give the same results. 
+2) When there are very large numbers, they are put into the denominator in the gen_omegas function. Dividing by a very large number is that same as multiplying by a very small number. when any small and large number are multiplied, they lose numerical significance. This is due to the numerical instability in how numbers are stored in base 2 registers in the computer. This problem can be mitigated with larger floating point types, but is never fully avoided.
+As n increases, the numerical instability increases. for example, if n is larger than 2^16, it can't be accurately represented with a 16 bit float. Python might be able to identify this and switch to a arbitrarily large float, but this still will lead to inaccuracies. This numerical error can result in an error in the integer solution
+3) The FFT multiply algorithm was by far the fastest. The results are shown in the graph "Graph_20_min_runtime.png". Note the different slopes of the various lines representing the big_O time complexity. For the FFT, it did n=2^18 in 30 minutes. For highschool, it did 2^16 in 54 minutes. For the three-sub algo, it did it n=2^18 in 38 minutes.
+4) The correct padding and correct handing of excessively large integers (larger than 2^32) are all handled by passing the list/array into the function "correct_printf"
+5) The code is well documented and explained
 
--- src/assn8_poly_multiply_fft/poly_multiply.py
+++ src/assn8_poly_multiply_fft/poly_multiply.py
@@ -1,5 +1,8 @@
 import numpy as np
-
+import time
+import sys
+import matplotlib.pyplot as plt
+import pathlib2 as path
 
 '''
 Creates a numpy array of int32 of size 2**n.
@@ -11,9 +14,9 @@ multiplication
 def create_int(n: int):
 
     if n > 20:  # to avoid very large numbers
-        return np.random.randint(0, 65534, np.power(2, n), np.uint64)
+        return np.random.randint(0, 65545, np.power(2, n), np.uint64)
     else:
-        return np.random.randint(0, 65534, np.power(2, n), np.uint64)
+        return np.random.randint(0, 65545, np.power(2, n), np.uint64)
 
 
 # high school math
@@ -53,7 +56,7 @@ def pad_printout(item):
     else:
         return
 
-
+# pads any list with zeros so that its len(arr) == 2^something
 def pad_power_2(arr):
     i = 0
     n = 0
@@ -70,7 +73,8 @@ def pad_power_2(arr):
     return arr
 
 # faster version of the four sub with some tricky algebra
-def three_sub_other(p: np.array, q: np.array, n: int) -> np.array:
+def three_sub_other(p: np.array, q: np.array) -> np.array:
+    n = len(p)
     arr = np.zeros(2*n, dtype=float)
 
     # recursion base case
@@ -81,13 +85,13 @@ def three_sub_other(p: np.array, q: np.array, n: int) -> np.array:
     # breaking the problem into sub problems. The big difference
     # from four_sub is that there are only three sub problem to
     # computes. Reduces time complexity from O(n^2) to O(n^1.5)
-    pq_ll = three_sub_other(p[:n//2], q[:n//2], n//2)
-    pq_hh = three_sub_other(p[n//2:], q[n//2:], n//2)
+    pq_ll = three_sub_other(p[:n//2], q[:n//2])
+    pq_hh = three_sub_other(p[n//2:], q[n//2:])
 
     # difference mid step
     p_mid = p[:n//2] + p[n//2:]
     q_mid = q[:n//2] + q[n//2:]
-    pq_mid = three_sub_other(p_mid, q_mid, n//2)
+    pq_mid = three_sub_other(p_mid, q_mid)
 
     # solution construction step
     arr[:n] = pq_ll
@@ -96,99 +100,146 @@ def three_sub_other(p: np.array, q: np.array, n: int) -> np.array:
 
     return arr
 
-def getV(n, sign=-1):
+# generates the omegas for the fft algo
+def gen_omegas(n, sign=-1):
     return np.array([complex(np.cos(2*np.pi*i/n), sign * np.sin(2*np.pi*i/n)) for i in range(0, n)])
 
-# TODO understand this code and switch the signs to make it into ifft
-def fft(p, v, n, depth=0):
-    #print("%s%s" % (" |"*depth+"in =", str(p)))
-    if n == 1:
-        #print("%s%s" % (" |"*depth+"out=", str(p)))
-        return p
-    # split into even and odd
-    eve = [p[i] for i in range(0, n, 2)]
-    odd = [p[i] for i in range(1, n, 2)]
-    # square the v values
-    v2 = [v[i]*v[i] for i in range(0, n//2)]
-    # solve the two sub problems
-    eveS = fft(eve, v2, n//2, depth+3)
-    oddS = fft(odd, v2, n//2, depth+3)
-    # construct the solution
-    solution = ([eveS[i] + v[i]*oddS[i] for i in range(0, n//2)] +
-                [eveS[i] - v[i]*oddS[i] for i in range(0, n//2)])
-    #print("%s%s" % (" |"*depth+"out=", str(solution)))
-    return solution
-
-
+# starts the fft function with only a given array
 def fft_helper(arr):
     l = len(arr)
-    return new_fft(arr, getV(l), l)
+    return fft(arr, gen_omegas(l), l)
 
-def new_fft(arr, w, n):
+# fast fourier transform. Assumes that arr is already of len 2^integer
+def fft(arr, w, n):
     if n == 1:
         return arr
+    # splits into even and odds
     even = arr[0::2]
     odd = arr[1::2]
     # square omega terms.
     # assumes it is type ndarray
     w2 = w*w
-    sol_even = new_fft(even, w2, n//2)
-    sol_odd = new_fft(odd, w2, n//2)
+    sol_even = fft(even, w2, n // 2)
+    sol_odd = fft(odd, w2, n // 2)
     pos = [sol_even[i] + w[i]*sol_odd[i] for i in range(n//2)]
     neg = [sol_even[i] - w[i]*sol_odd[i] for i in range(n//2)]
     solution = pos + neg
     return solution
 
+# inverse fast fourier transform
 def ifft(p):
     length = len(p)
-    return np.array(fft(p, getV(length, -1), length))/length
+    return np.array(fft(p, gen_omegas(length, 1), length)) / length
 
-
+# utilizes the fft algorithm above to quickly multiply two large integers.
+# output should match high school algorithm and three_sub algorithm
 def fft_multiply(arr1, arr2):
+    # pad the arrays to a len of a power of 2
     arr1 = pad_power_2(arr1)
     arr2 = pad_power_2(arr2)
     assert len(arr1) == len(arr2)
 
-    phase_space_arr_1 = fft(arr1, getV(len(arr1)), len(arr1))
-    phase_space_arr_2 = fft(arr2, getV(len(arr2)), len(arr2))
+    # turn both arrays into phase space
+    phase_space_arr_1 = np.array(fft(arr1, gen_omegas(len(arr1)), len(arr1)))
+    phase_space_arr_2 = np.array(fft(arr2, gen_omegas(len(arr2)), len(arr2)))
 
-    whole_phase_space_arr = [phase_space_arr_1[i]*phase_space_arr_2[i] for i in range(len(arr1))]
+    # element wise multiplication in phase space
+    total_phase_space_arr = phase_space_arr_1*phase_space_arr_2
 
-    whole_real_space_arr = ifft(whole_phase_space_arr)
+    # turn back into real space
+    total_real_space_arr = ifft(total_phase_space_arr)
 
-    multiplied_coefficients = [round(num.real) for num in whole_real_space_arr]
+    # takes the integer real part of the complex numbers
+    multiplied_coefficients = list(map(lambda x: x.real, total_real_space_arr))
 
     return multiplied_coefficients
 
+# runs a multiplication problem on larger and larger problem sets until it reaches 20 minutes of CPU time.
+# returns a list of size n vs time
+def run_for_twenty_minutes(function):
+    size = 6
+    sizes = []
+    cpu_times = []
+    start = 0
+    runtime = 0.0
+    TIME_CUTOFF = 20*60
+    while runtime < TIME_CUTOFF:
+        sizes.append(size)
+        start = time.time()
+        arr = create_int(size)
+        arr2 = create_int(size)
+        function(arr, arr2)
+        runtime = time.time() - start
+        cpu_times.append(runtime)
+        size += 1
 
-def gen_omegas(n, inverse = False):
-  angle = 0
-  delt = 2*np.pi/n
-  omegas = []
-  for _ in range(0, n+1):
-    if inverse:
-      omegas.append(np.cos(angle) - 1j*np.sin(angle))
-    else:
-      omegas.append(np.cos(angle) + 1j*np.sin(angle))
-    angle += delt
-  return omegas
+    return sizes, cpu_times
 
+# during the multiplication process, the size of ints could have exceeded
+# 4294967295. In this case, that int should have 2^32 subtracted from it
+# and one can be added to the next cell. Highest orders are first
+def carry_over_overflow(arr):
+    MAX = 4294967296
+
+    # adding on buffer of 1 in case the highest cell overflows
+    if isinstance(arr, np.ndarray) and arr[0] != 0:
+        arr = np.concatenate([np.zeros((1,)), arr])
+    elif isinstance(arr, list) and arr[0] != 0:
+        arr = [0] + arr
+
+    for i in range(len(arr) - 1, 0, -1):
+        if arr[i] >= MAX:
+            while arr[i] >= MAX:
+                arr[i] -= MAX
+                arr[i-1] += 1
+    return arr
+
+# takes care of the overflow of int 2^32 and string padding
+def correct_printf(arr):
+    return pad_printout(carry_over_overflow(arr))
 
 
 if __name__ == "__main__":
     size = 2
     arr = create_int(size)
+    print(f"arr : {arr}")
     arr2 = create_int(size)
-    print('results of three sub:   ', pad_printout(three_sub_other(arr, arr2, 2 ** size)))
-    print('results of high school: ', pad_printout(foil_multiply(arr, arr2)))
-    print('results of fft multiply:', pad_printout(fft_multiply(arr, arr2)))
+    print('results of three sub:   ', correct_printf(three_sub_other(arr, arr2)))
+    print('results of high school: ', correct_printf(foil_multiply(arr, arr2)))
+    print('results of fft multiply:', correct_printf(fft_multiply(arr, arr2)))
 
-    print("my fft: ", fft(arr, getV(2**size), 2**size))
-    print("np fft: ", np.fft.fft(arr))
-    print("my new fft: ", fft_helper(arr))
+    if len(sys.argv) > 1 and sys.argv[1] == "-r":
+        print("running time out function...")
+        if sys.argv[2] == "three":
+            print(f"results for three sub max runtime\n {run_for_twenty_minutes(three_sub_other)}")
+        elif sys.argv[2] == "fft":
+            print(f"results for fft multiply max runtime\n {run_for_twenty_minutes(fft_multiply)}")
+        elif sys.argv[2] == "hs":
+            print(f"results for high school max runtime\n {run_for_twenty_minutes(foil_multiply)}")
+        else:
+            pass
 
+    fft_results = ([6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18],
+                   [0.001993417739868164, 0.00498652458190918, 0.007978677749633789, 0.018457412719726562,
+                    0.04091620445251465, 0.09674215316772461, 0.25830793380737305, 0.991936445236206,
+                    4.190894365310669, 21.34105157852173, 104.79319977760315, 413.393187046051, 1749.1212944984436])
+    hs_results = ([6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
+                  [0.0029914379119873047, 0.012471437454223633, 0.0468747615814209, 0.19447946548461914,
+                   0.772932767868042, 3.2929346561431885, 13.544007301330566, 49.57841515541077, 202.50718665122986,
+                   839.6248352527618, 3248.8114218711853])
+    three_sub_results =  ([6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18],
+                          [0.003988981246948242, 0.010964393615722656, 0.034920454025268555, 0.10271143913269043,
+                           0.304180383682251, 0.9285223484039307, 2.7829794883728027, 9.199534177780151,
+                           28.146015644073486, 83.44644165039062, 242.09331274032593, 777.3618574142456, 2276.2384588718414])
 
-    print(getV(4))
-    print(gen_omegas(4))
-
-
+    # display graph for
+    plt.plot(fft_results[0], fft_results[1], label="FFT")
+    plt.plot(three_sub_results[0], three_sub_results[1], label="3-Sub")
+    plt.plot(hs_results[0], hs_results[1], label="High School")
+    plt.title("N reached after 20 Minutes of CPU Time\n Problem size 2^n vs Time")
+    plt.xlabel("n")
+    plt.ylabel("Seconds (log 2)")
+    plt.yscale('log', basey=2)
+    plt.legend()
+    # plt.savefig(path.Path.joinpath(path.Path.cwd(), 'Graph_20_min_runtime.png'))
+    # plt.show()
