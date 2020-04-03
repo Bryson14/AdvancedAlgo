4ef863c5ec9b9f4f6de4c25ada7835594367e251
Bryson <bryson.meiling@aggiemail.usu.edu>
2020-04-02 21:20:00 -0600

    why does my fft have some different negatives that np.fft

--- src/assn8_poly_multiply_fft/poly_multiply.py
+++ src/assn8_poly_multiply_fft/poly_multiply.py
@@ -11,9 +11,9 @@ multiplication
 def create_int(n: int):
 
     if n > 20:  # to avoid very large numbers
-        return np.random.randint(0, 65534, np.power(2, n), np.uint32)
+        return np.random.randint(0, 65534, np.power(2, n), np.uint64)
     else:
-        return np.random.randint(0, 65534, np.power(2, n), np.uint32)
+        return np.random.randint(0, 65534, np.power(2, n), np.uint64)
 
 
 # high school math
@@ -35,7 +35,7 @@ def pad_printout(item):
     if isinstance(item, np.ndarray) or isinstance(item, list):
         output = ""
         for num in item:
-            num = str(num)
+            num = str(int(num))
             num = (10-len(num))*"0" + num
             output += num + ", "
 
@@ -52,6 +52,22 @@ def pad_printout(item):
 
     else:
         return
+
+
+def pad_power_2(arr):
+    i = 0
+    n = 0
+    while n != len(arr):
+        n = 2 ** i
+        # requires a buffer to have len of power of 2
+        if n > len(arr):
+            return np.concatenate((arr, np.zeros([n - len(arr), ])))
+
+        # N is still smaller than arr length
+        else:
+            i += 1
+
+    return arr
 
 # faster version of the four sub with some tricky algebra
 def three_sub_other(p: np.array, q: np.array, n: int) -> np.array:
@@ -80,11 +96,10 @@ def three_sub_other(p: np.array, q: np.array, n: int) -> np.array:
 
     return arr
 
+def getV(n, sign=1):
+    return np.array([complex(np.cos(2*np.pi*i/n), sign * np.sin(2*np.pi*i/n)) for i in range(0, n)])
 
-def getV(n, sign = 1):
-    return [complex(np.cos(2*np.pi*i/n), sign * np.sin(2*np.pi*i/n)) for i in range(0, n)]
-
-# TODO undertand this code and switch the signs to make it into ifft
+# TODO understand this code and switch the signs to make it into ifft
 def fft(p, v, n, depth = 0):
     #print("%s%s" % (" |"*depth+"in =", str(p)))
     if n == 1:
@@ -104,3 +119,56 @@ def fft(p, v, n, depth = 0):
     #print("%s%s" % (" |"*depth+"out=", str(solution)))
     return solution
 
+
+def fft_helper(arr):
+    l = len(arr)
+    return new_fft(arr, getV(l), l)
+
+def new_fft(arr, w, n):
+    if n == 1:
+        return arr
+    even = arr[0::2]
+    odd = arr[1::2]
+    # square omega terms.
+    # assumes it is type ndarray
+    w2 = w*w
+    sol_even = new_fft(even, w2, n//2)
+    sol_odd = new_fft(odd, w2, n//2)
+    pos = [sol_even[i] + w[i]*sol_odd[i] for i in range(n//2)]
+    neg = [sol_even[i] - w[i]*sol_odd[i] for i in range(n//2)]
+    solution = pos + neg
+    return solution
+
+def ifft(p):
+    length = len(p)
+    return np.array(fft(p, getV(length, -1), length))/length
+
+
+def fft_multiply(arr1, arr2):
+    arr1 = pad_power_2(arr1)
+    arr2 = pad_power_2(arr2)
+    assert len(arr1) == len(arr2)
+
+    phase_space_arr_1 = fft(arr1, getV(len(arr1)), len(arr1))
+    phase_space_arr_2 = fft(arr2, getV(len(arr2)), len(arr2))
+
+    whole_phase_space_arr = [phase_space_arr_1[i]*phase_space_arr_2[i] for i in range(len(arr1))]
+
+    whole_real_space_arr = ifft(whole_phase_space_arr)
+
+    multiplied_coefficients = [round(num.real) for num in whole_real_space_arr]
+
+    return multiplied_coefficients
+
+
+if __name__ == "__main__":
+    size = 2
+    arr = create_int(size)
+    arr2 = create_int(size)
+    print('results of three sub:   ', pad_printout(three_sub_other(arr, arr2, 2 ** size)))
+    print('results of high school: ', pad_printout(foil_multiply(arr, arr2)))
+    print('results of fft multiply:', pad_printout(fft_multiply(arr, arr2)))
+
+    print("my fft: ", fft(arr, getV(2**size), 2**size))
+    print("np fft: ", np.fft.fft(arr))
+    print("my new fft: ", fft_helper(arr))
